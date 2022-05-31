382924f4bf087e99d3f4f6b5ab7d31719aa38986
Taesik Jo <67845112+r4pidstart@users.noreply.github.com>
2022-06-01 03:51:49 +0900

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -16,8 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	cur;
 
-	if (!dst && !src)
-		return (0);
+	if (dst == src)
+		return (dst);
 	cur = -1;
 	while (++cur < n)
 		*(char *)(dst + cur) = *((char *)src + (cur));
